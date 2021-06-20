5e3f5033a7b6aa177da0034e2d1d3662c09e78ad
FelipeFerreir19 <82649613+Delarmelina@users.noreply.github.com>
2021-06-20 14:28:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "triangulo.h"
+
+using namespace std;
+using namespace TT;
+
+int main() {
+  tTriangulos T1;
+
+  cout << "Lado 1: ";
+  cin >> T1.lado1;  //Cateto1
+  cout << "Lado 2: ";
+  cin >> T1.lado2;  //Cateto2
+
+  T1.lado3 = calcularHipotenusa(T1);
+  calcularPerimetro(T1);
+  calcularArea(T1); 
+
+  cout << "Hipotenusa do triangulo: " << T1.lado3 << endl;
+}
